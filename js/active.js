f557cb919a598457cf8e866518d5ccb65ee54c65
Jesus Mendoza <jesusmdz929@gmail.com>
2024-03-20 23:25:46 -0600

    feature(project):project for EducaUno has been created and finished

---
+++ js/active.js
@@ -0,0 +1,16 @@
+const activePage = window.location.pathname;
+const navLink = document.querySelectorAll("nav a");
+navLink.forEach((link) => {
+  if (link.href.includes(activePage)) {
+    link.classList.add("active");
+  }
+});
+
+function myFunction() {
+  var x = document.getElementById("nav-style");
+  if (x.className === "nav-style") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-style";
+  }
+}
